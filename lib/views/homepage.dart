5675b0c9d800845dcaf198d0e578b89c506a2254
kingace2056 <sarthak123parajuli@gmail.com>
2022-12-18 17:04:32 +0545

    search functionality added

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -19,6 +19,8 @@ class _HomePageState extends State<HomePage> {
   UserService userService = UserService();
 
   List<UserModel> userList = [];
+  List<UserModel> tempList = [];
+  List<UserModel> searchList = [];
   @override
   void initState() {
     // TODO: implement initState
@@ -28,6 +30,7 @@ class _HomePageState extends State<HomePage> {
 
   fetchUsers() async {
     userList = await userService.getUsers(context: context);
+    tempList = userList;
     setState(() {});
   }
 
@@ -40,80 +43,128 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ListView.builder(
-              itemCount: userList.isEmpty ? 5 : userList.length,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (BuildContext context, int index) {
-                return userList.isEmpty
-                    ? Shimmer.fromColors(
-                        child: Container(
-                          height: 30,
-                        ),
-                        baseColor: Colors.grey,
-                        highlightColor: Colors.blueGrey)
-                    : Container(
-                        margin: const EdgeInsets.all(15),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: primaryColor),
-                        ),
-                        height: MediaQuery.of(context).size.height * 0.11,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  userList[index].name.toString(),
-                                  style: TextStyle(
-                                      color: primaryColor,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
-                                ),
-                                Text(
-                                  '@' + userList[index].username.toString(),
-                                  style: TextStyle(
-                                      color: Color.fromARGB(255, 131, 131, 131),
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 15),
-                                ),
-                                Text(
-                                  userList[index].company!.name.toString(),
-                                  style: TextStyle(
-                                      color: Color.fromARGB(255, 86, 86, 86),
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 15),
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Center(
-                                child: IconButton(
-                              onPressed: () {
-                                log('Pressed');
-                                showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return showAlert(
-                                        userModel: userList[index],
-                                      );
-                                    });
-                              },
-                              icon: Icon(
-                                Icons.info,
-                                color: Colors.blueAccent,
-                                size: 30,
+            Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextFormField(
+                onFieldSubmitted: (value) {
+                  searchList.clear();
+                  for (int i = 0; i < tempList.length; i++) {
+                    if (tempList[i]
+                        .name!
+                        .toLowerCase()
+                        .contains(value.toLowerCase())) {
+                      searchList.add(tempList[i]);
+                    }
+                    userList = searchList;
+                    setState(() {});
+                  }
+                },
+                onChanged: (value) {
+                  searchList.clear();
+                  for (int i = 0; i < tempList.length; i++) {
+                    if (tempList[i]
+                        .name!
+                        .toLowerCase()
+                        .contains(value.toLowerCase())) {
+                      searchList.add(tempList[i]);
+                    }
+                    userList = searchList;
+                    setState(() {});
+                  }
+                },
+                decoration: const InputDecoration(
+                    hintText: 'Search',
+                    icon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(0)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.builder(
+                itemCount: userList.isEmpty ? 5 : userList.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return userList.isEmpty
+                      ? Shimmer.fromColors(
+                          child: Container(
+                            height: 30,
+                          ),
+                          baseColor: Colors.grey,
+                          highlightColor: Colors.blueGrey)
+                      : Container(
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.only(bottom: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: primaryColor),
+                          ),
+                          height: MediaQuery.of(context).size.height * 0.11,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    userList[index].name.toString(),
+                                    style: TextStyle(
+                                        color: primaryColor,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Text(
+                                    '@' + userList[index].username.toString(),
+                                    style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(255, 131, 131, 131),
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 15),
+                                  ),
+                                  Text(
+                                    userList[index].company!.name.toString(),
+                                    style: const TextStyle(
+                                        color: Color.fromARGB(255, 86, 86, 86),
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 15),
+                                  ),
+                                ],
                               ),
-                            ))
-                          ],
-                        ),
-                      );
-              },
+                              const Spacer(),
+                              Center(
+                                  child: IconButton(
+                                onPressed: () {
+                                  log('Pressed');
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return showAlert(
+                                          userModel: userList[index],
+                                        );
+                                      });
+                                },
+                                icon: const Icon(
+                                  Icons.info,
+                                  color: Colors.blueAccent,
+                                  size: 30,
+                                ),
+                              ))
+                            ],
+                          ),
+                        );
+                },
+              ),
             ),
           ],
         ),
